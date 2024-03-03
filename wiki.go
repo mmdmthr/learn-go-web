164e63cc1d974bbb544c8bc39204fa8f0a24b0d4
mmdmthr <muhhari@gmail.com>
2024-03-03 15:01:57 +0700

    write and read file

---
+++ wiki.go
@@ -0,0 +1,41 @@
+package main
+
+import (
+    "fmt"
+    "os"
+    "log"
+    "net/http"
+)
+
+type Page struct {
+    Title string
+    Body []byte
+}
+
+func (p *Page) save() error {
+    filename := p.Title + ".txt"
+    return os.WriteFile(filename, p.Body, 0600)
+}
+
+func loadPage(title string) (*Page, error) {
+    filename := title + ".txt"
+    body, err := os.ReadFile(filename)
+    if err != nil {
+        return nil, err
+    }
+    return &Page{Title: title, Body: body}, nil
+}
+
+func viewHandler(w http.ResponseWriter, r *http.Request) {
+    title := r.URL.Path[len("/view/");]
+    p, _ := loadPage(title)
+    fmt.Fprintf(w, "<h1>%s</h1><div>%s</div>", p.Title, p.Body)
+}
+
+func main() {
+    p1 := &Page{Title: "TestPage", Body: []byte("This is a sample Page.")}
+    p1.save()
+    p2, _ := loadPage("TestPage")
+    fmt.Println(string(p2.Body))
+}
+
