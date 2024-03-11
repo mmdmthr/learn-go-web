0aa8e5faa90ac2f2e6bc5440c16492cf64391fe4
mmdmthr <muhhari@gmail.com>
2024-03-11 13:07:33 +0700

    store templates in tmpl and page data in data

--- wiki.go
+++ wiki.go
@@ -14,12 +14,12 @@ type Page struct {
 }
 
 func (p *Page) save() error {
-    filename := p.Title + ".txt"
+    filename := "data/" + p.Title + ".txt"
     return os.WriteFile(filename, p.Body, 0600)
 }
 
 func loadPage(title string) (*Page, error) {
-    filename := title + ".txt"
+    filename := "data/" + title + ".txt"
     body, err := os.ReadFile(filename)
     if err != nil {
         return nil, err
@@ -55,7 +55,7 @@ func saveHandler(w http.ResponseWriter, r *http.Request, title string) {
 	http.Redirect(w, r, "/view/"+title, http.StatusFound)
 }
 
-var templates = template.Must(template.ParseFiles("edit.html", "view.html"))
+var templates = template.Must(template.ParseFiles("tmpl/edit.html", "tmpl/view.html"))
 
 func renderTemplate(w http.ResponseWriter, tmpl string, p *Page) {
 	err := templates.ExecuteTemplate(w, tmpl+".html", p)
